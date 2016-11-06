8cbf7f406853731690dbd23060bbb43d95a7275a
tsun1650 <tonysun996@gmail.com>
2016-11-06 15:29:55 -0500

    Show Filtered Students in Excel
    Filtered students show up in a spreadsheet

--- WindowsFormsApplication1/WindowsFormsApplication1/Filter.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Filter.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace WindowsFormsApplication1
 {
@@ -29,11 +32,7 @@ namespace WindowsFormsApplication1
             Race.DataSource = Model.getRaces();
             Current.DataSource = Model.getCurrentStudentOptions();
 
-            /*Gender.Items.Add("All");
-            School.Items.Add("All");
-            Race.Items.Add("All");
-            Current.Items.Add("All");
-              */          
+               
             Gender.DropDownStyle = ComboBoxStyle.DropDownList;
             School.DropDownStyle = ComboBoxStyle.DropDownList;
             Race.DropDownStyle = ComboBoxStyle.DropDownList;
@@ -48,8 +47,8 @@ namespace WindowsFormsApplication1
             int maxGrade = Int32.Parse(Grade2.Text);
             int minBehavior = Int32.Parse(Behavior1.Text);
             int maxBehavior = Int32.Parse(Behavior2.Text);
-            int minAttendence = Int32.Parse(Attend1.Text);
-            int maxAttendence = Int32.Parse(Attend2.Text);
+            int minAttendance = Int32.Parse(Attend1.Text);
+            int maxAttendance = Int32.Parse(Attend2.Text);
 
             List<string> genders = new List<string>();
             genders.Add(Gender.GetItemText(Gender.SelectedItem));
@@ -59,16 +58,83 @@ namespace WindowsFormsApplication1
             races.Add(Gender.GetItemText(Race.SelectedItem));
             List<string> statuses = new List<string>();
             statuses.Add(Current.GetItemText(Current.SelectedItem));
+            
+            List<string>[] selectedVals= Model.filterSelectStudent(minGPA, maxGPA, schools, minGrade, maxGrade, minBehavior, maxBehavior, minAttendance, maxAttendance, genders, races, statuses);
 
-            List<string>[] selectedVals= Model.filterSelectStudent(minGPA, maxGPA, schools, minGrade, maxGrade, minBehavior, maxBehavior, minAttendence, maxAttendence, genders, races, statuses);
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            Excel.Range oRng;
 
-            if (Model.OpenConnection() == true)
+            
+            try
             {
-                this.Hide();
-                Model.CloseConnection();
-                //Main nextForm = new Main();
-                //nextForm.ShowDialog();
-                this.Close();
+                //Start Excel and get Application object.
+                oXL = new Excel.Application();
+                oXL.Visible = false;
+
+                //Get a new workbook. Set up worksheets
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+                
+
+                //Add table headers going cell by cell.
+                oSheet.Cells[1, 1] = "ID";
+                oSheet.Cells[1, 2] = "First Name";
+                oSheet.Cells[1, 3] = "Last Name";
+                oSheet.Cells[1, 4] = "Grade Level";
+                oSheet.Cells[1, 5] = "Grade Modified Date";
+                oSheet.Cells[1, 6] = "Registration Date";
+                oSheet.Cells[1, 7] = "Gender";
+                oSheet.Cells[1, 8] = "Race";
+                oSheet.Cells[1, 9] = "Current?";
+                oSheet.Cells[1, 10] = "Days Missed";
+                oSheet.Cells[1, 11] = "GPA";
+                oSheet.Cells[1,12] = "GPA Entry Date";
+                oSheet.Cells[1,13] = "Start Date";
+                oSheet.Cells[1,14] = "End Date";
+                oSheet.Cells[1,15] = "School Name";
+                oSheet.Cells[1,16] = "Referral Count";
+
+                //Format A1:P1 as bold, vertical alignment = center.
+                oSheet.get_Range("A1", "P1").Font.Bold = true;
+                oSheet.get_Range("A1", "P1").VerticalAlignment =
+                    Excel.XlVAlign.xlVAlignCenter;
+
+                // Create an array to multiple values at once.
+                string[] toPrint = new string[selectedVals.Count()];
+
+
+                for (int i = 0; i < selectedVals.Count(); i = i + 1)
+                {
+                    toPrint[i] = selectedVals[i][0];
+                }
+
+                //Fill A2:B6 with an array of values (First and Last Names).
+                oSheet.get_Range("A2", "P2").Value2 = toPrint;
+
+                //AutoFit columns A:P.
+                oRng = oSheet.get_Range("A1", "P1");
+                oRng.EntireColumn.AutoFit();
+
+
+
+                
+
+                //Make sure Excel is visible and give the user control
+                //of Microsoft Excel's lifetime.
+                oXL.Visible = true;
+                oXL.UserControl = true;
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
             }
 
         }
