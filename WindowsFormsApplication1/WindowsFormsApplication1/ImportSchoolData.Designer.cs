3e0d2a48f8dee018f3939a1a32eb61085e838bbf
Nija <nijakurien@gmail.com>
2016-11-15 01:09:56 -0500

    Add labels to importschool data

--- WindowsFormsApplication1/WindowsFormsApplication1/ImportSchoolData.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/ImportSchoolData.Designer.cs
@@ -31,13 +31,19 @@
             this.importButton = new System.Windows.Forms.Button();
             this.subButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // importButton
             // 
-            this.importButton.Location = new System.Drawing.Point(119, 110);
+            this.importButton.Location = new System.Drawing.Point(80, 116);
+            this.importButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.importButton.Name = "importButton";
-            this.importButton.Size = new System.Drawing.Size(232, 74);
+            this.importButton.Size = new System.Drawing.Size(155, 48);
             this.importButton.TabIndex = 0;
             this.importButton.Text = "Import File";
             this.importButton.UseVisualStyleBackColor = true;
@@ -45,9 +51,11 @@
             // 
             // subButton
             // 
-            this.subButton.Location = new System.Drawing.Point(119, 274);
+            this.subButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subButton.Location = new System.Drawing.Point(80, 215);
+            this.subButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.subButton.Name = "subButton";
-            this.subButton.Size = new System.Drawing.Size(231, 63);
+            this.subButton.Size = new System.Drawing.Size(154, 41);
             this.subButton.TabIndex = 1;
             this.subButton.Text = "Submit";
             this.subButton.UseVisualStyleBackColor = true;
@@ -56,20 +64,77 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(191, 47);
+            this.label1.Location = new System.Drawing.Point(127, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Import Multiple Students";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Click the \"Import File\" button first. Copy and Paste the data";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "from the provided template. You may copy the headers";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "onto the blank Excel sheet that pops up.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(2, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(330, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Once data is copied to blank Excel file, save the file and click submit";
+            // 
             // ImportSchoolData
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 441);
+            this.ClientSize = new System.Drawing.Size(342, 326);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.subButton);
             this.Controls.Add(this.importButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ImportSchoolData";
             this.Text = "ImportSchoolData";
             this.ResumeLayout(false);
@@ -82,5 +147,10 @@
         private System.Windows.Forms.Button importButton;
         private System.Windows.Forms.Button subButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
